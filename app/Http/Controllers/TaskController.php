70a7b273f932c5d876874c47d8c45a2d9e54eed3
眞壁大二朗 <daijiromakabe@makabedaijirous-MacBook-Air.local>
2021-05-16 19:31:23 +0900

    final commit

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Folder;
+use App\Models\Task;
+use App\Http\Requests\CreateTask;
+use Illuminate\Http\Request;
+use App\Http\Requests\EditTask;
+// ★ Authクラスをインポートする
+use Illuminate\Support\Facades\Auth;
+
+
+class TaskController extends Controller
+{
+    //
+    public function index(int $id)
+{
+    // if (Auth::user()->id !== $id->user_id) {
+    //     abort(403);
+    // }
+     // ★ ユーザーのフォルダを取得する
+     $folders = Auth::user()->folders()->get();
+    // 選ばれたフォルダを取得する
+    $current_folder = Folder::find($id);
+    if (is_null($current_folder)) {
+        abort(404);
+    }
+// dd($current_folder);
+// var_dump($current_folder);
+    // 選ばれたフォルダに紐づくタスクを取得する
+    $tasks = $current_folder->tasks()->get(); // ★
+    // $tasks = Task::where('folder_id', $current_folder->id)->get();
+
+    return view('tasks/index', [
+        'folders' => $folders,
+        'current_folder_id' => $current_folder->id,
+        'tasks' => $tasks,
+    ]);
+}
+/**
+ * GET /folders/{id}/tasks/create
+ */
+public function showCreateForm(int $id)
+{
+    return view('tasks/create', [
+        'folder_id' => $id
+    ]);
+}
+public function create(int $id, CreateTask $request)
+{
+    $current_folder = Folder::find($id);
+
+    $task = new Task();
+    $task->title = $request->title;
+    $task->due_date = $request->due_date;
+
+    $current_folder->tasks()->save($task);
+
+    return redirect()->route('tasks.index', [
+        'id' => $current_folder->id,
+    ]);
+}
+/**
+ * GET /folders/{id}/tasks/{task_id}/edit
+ */
+public function showEditForm(int $id, int $task_id)
+{
+    // if ($id !== $folder_id) {
+    //     abort(404);
+    // }
+    $task = Task::find($task_id);
+
+    return view('tasks/edit', [
+        'task' => $task,
+    ]);
+}
+public function edit(int $id, int $task_id, EditTask $request)
+{
+    // if ($id !== $folder_id) {
+    //     abort(404);
+    // }
+    // 1
+    $task = Task::find($task_id);
+
+    // 2
+    $task->title = $request->title;
+    $task->status = $request->status;
+    $task->due_date = $request->due_date;
+    $task->save();
+
+    // 3
+    return redirect()->route('tasks.index', [
+        'id' => $task->folder_id,
+    ]);
+}
+}
